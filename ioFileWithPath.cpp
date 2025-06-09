f67e75162e4e4644ffbc17072485aaa5e14bc5dd
NurulNajatMony <nurulmony505@gmail.com>
2025-06-09 11:31:27 +0700

    Menambahkan input nama file dari pengguna

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -4,3 +4,16 @@
 using namespace std;
 
 
+int main() {
+    string baris;
+    string namaFile;
+
+    cout << "Masukkan nama file: ";
+    cin >> namaFile;
+
+    // operasi file dalam mode  menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untuk membuka
+    outfile.open(namaFile + ".txt");
+    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
