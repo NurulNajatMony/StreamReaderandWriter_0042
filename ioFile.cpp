255a5943bdd1d58643d397d80ee0411a55bfade8
NurulNajatMony <nurulmony505@gmail.com>
2025-06-09 10:47:20 +0700

    Menambahkan pembuka file dan pesan panduan input

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,3 +3,14 @@
 #include <string>
 using namespace std;
 
+
+int main() {
+    string baris;
+
+    // operasi file dalam mode  menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untuk membuka
+    outfile.open("contohfile.txt");
+    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
